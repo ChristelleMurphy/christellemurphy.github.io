d2088f6d451302ea946d2e8474c3906726045155
Christelle Murphy <c00253022@itcarlow.ie>
2021-02-24 10:20:35 +0000

    fixed splitfunction

--- js/register.js
+++ js/register.js
@@ -1,6 +1,8 @@
 window.onload = () => {
     'use strict';
   
+    splitFunction();
+
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker
                .register('./service_worker.js');

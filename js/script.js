38958437df4008b9fc6ce94f54ffe0dc568a9ba3
Christelle Murphy <c00253022@itcarlow.ie>
2021-03-04 19:13:22 +0000

    added paragraph

--- js/script.js
+++ js/script.js
@@ -313,7 +313,7 @@ function splitFunction() {
     console.log("split");
     var url = document.location.href;
     var result = url.split("="); // Splits string based on =
-    document.getElementById("gamerTag").innerHTML = "Welcome " + result[1];
+    document.getElementById("gamerTag").innerHTML = result[1];
     }
 
 function gameloop() {
